a911c1718b14bf1f8fb0b19fe3e9cdb9fc9d4086
thomasmichaelwallace <thomasmichaelwallace@gmail.com>
2021-03-21 15:33:03 +0000

    feat: parse trid magic database

---
+++ scripts/parseMagic.js
@@ -0,0 +1,43 @@
+/* eslint-disable no-console */
+const fs = require('fs');
+const path = require('path');
+const parser = require('xml2json');
+
+const DEFS_ROOT = './resources/unknown/defs';
+const DEFS_OUT = `${DEFS_ROOT}/db.json`;
+
+const PARSER_OPTIONS = {
+  object: true,
+  coerce: false,
+  sanitize: true,
+  trim: true,
+  arrayNotation: true,
+  alternateTextNode: false,
+};
+
+const magic = { /* [up-to first four bytes]: names[] */ };
+
+const dirs = fs.readdirSync(DEFS_ROOT);
+dirs.forEach((d) => {
+  const files = fs.readdirSync(path.join(DEFS_ROOT, d)).map((f) => path.join(DEFS_ROOT, d, f));
+  files.forEach((f) => {
+    const data = fs.readFileSync(f);
+    const { TrID: [xml] } = parser.toJson(data, PARSER_OPTIONS);
+
+    if (xml.Info.length > 1) throw new TypeError(`Too many Info nodes ${f}`);
+    const { Info: [{ FileType: fileType, Ext: ext }] } = xml;
+    const description = `${fileType} (${ext})`;
+
+    const { FrontBlock: [{ Pattern: patterns }] } = xml;
+    const bytes = patterns.map((p) => p.Bytes[0]);
+
+    bytes.forEach((b) => {
+      magic[b] = magic[b] || [];
+      magic[b].push(description);
+    });
+
+    console.log(`added ${description}: ${bytes}`);
+  });
+});
+
+fs.writeFileSync(DEFS_OUT, JSON.stringify(magic, null, 2));
